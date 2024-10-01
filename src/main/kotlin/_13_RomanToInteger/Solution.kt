499b3e461f5c6fb13dc1967d4897732fdc99163e
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-10-01 08:22:19 +0300

    13. Roman to Integer

---
+++ src/main/kotlin/_13_RomanToInteger/Solution.kt
@@ -0,0 +1,108 @@
+package _13_RomanToInteger
+
+fun main() {
+    val solution = Solution()
+
+    println(solution.romanToInt("III")) // 3
+    println(solution.romanToInt("LVIII")) // 58
+    println(solution.romanToInt("MCMXCIV")) // 1994
+}
+
+class Solution {
+
+    fun romanToInt(s: String): Int {
+        val length = s.length
+        if (length < 1) return 0
+
+        var sum = 0
+        var index = 0
+
+        while (index < length) {
+            val one: Char = s[index]
+            val two: String = if (index + 1 < length) s.substring(index, index + 2) else ""
+
+            if (doubleRoman.contains(two)) {
+                sum += doubleRoman.getValue(two)
+                index++
+            } else if (singleRoman.contains(one)) {
+                sum += singleRoman.getValue(one)
+            }
+
+            index++
+        }
+
+        return sum
+    }
+
+    private val doubleRoman = mapOf<String, Int>(
+        "IV" to 4,
+        "IX" to 9,
+        "XL" to 40,
+        "XC" to 90,
+        "CD" to 400,
+        "CM" to 900
+    )
+
+    private val singleRoman = mapOf<Char, Int>(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000
+    )
+}
+
+/**
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ *
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * For example, 2 is written as II in Roman numeral, just two ones added together.
+ * 12 is written as XII, which is simply X + II.
+ * The number 27 is written as XXVII, which is XX + V + II.
+ *
+ * Roman numerals are usually written largest to smallest from left to right.
+ * However, the numeral for four is not IIII.
+ * Instead, the number four is written as IV.
+ * Because the one is before the five we subtract it making four.
+ * The same principle applies to the number nine, which is written as IX.
+ * There are six instances where subtraction is used:
+ *
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ *
+ * Example 1:
+ *
+ * Input: s = "III"
+ * Output: 3
+ * Explanation: III = 3.
+ *
+ * Example 2:
+ *
+ * Input: s = "LVIII"
+ * Output: 58
+ * Explanation: L = 50, V= 5, III = 3.
+ *
+ * Example 3:
+ *
+ * Input: s = "MCMXCIV"
+ * Output: 1994
+ * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 15
+ * s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+ * It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+ */
