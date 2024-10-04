8f126448a61a3112306aef9dac08f3fbf0bf42b1
Oleh Kholiavchuk <oleh.khol@gmail.com>
2024-10-04 08:03:13 +0300

    205. Isomorphic Strings

---
+++ src/main/kotlin/_205_IsomorphicStrings/Solution.kt
@@ -0,0 +1,116 @@
+package _205_IsomorphicStrings
+
+fun main() {
+    val solution = Solution()
+
+    println(solution.isIsomorphic(s = "13", t = "42"))
+    println(solution.isIsomorphic(s = "egg", t = "add"))
+    println(solution.isIsomorphic(s = "foo", t = "bar"))
+    println(solution.isIsomorphic(s = "paper", t = "title"))
+}
+
+class Solution {
+    fun isIsomorphic(s: String, t: String): Boolean {
+        val len = s.length
+        if (len == 0 || len != t.length) return false
+
+        val sArr = CharArray(128)
+        val tArr = CharArray(128)
+
+        for (i in 0 until len) {
+            val sInd = s[i].code
+            if (sArr[sInd] == '\u0000') {
+                sArr[sInd] = t[i]
+            } else if (sArr[sInd] != t[i]) {
+                return false
+            }
+
+            val tInd = t[i].code
+            if (tArr[tInd] == '\u0000') {
+                tArr[tInd] = s[i]
+            } else if (tArr[tInd] != s[i]) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
+
+class SolutionMaps {
+    fun isIsomorphic(s: String, t: String): Boolean {
+        val len = s.length
+        if (len == 0 || len != t.length) return false
+
+        val mapST = hashMapOf<Char, Char>()
+        val mapTS = hashMapOf<Char, Char>()
+
+        for (i in s.indices) {
+            val charS = s[i]
+            val charT = t[i]
+
+            if (mapST[charS] == null) {
+                mapST[charS] = charT
+            } else if (mapST[charS] != charT) {
+                return false
+            }
+
+            if (mapTS[charT] == null) {
+                mapTS[charT] = charS
+            } else if (mapTS[charT] != charS) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
+
+/**
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+ *
+ * All occurrences of a character must be replaced with another
+ * character while preserving the order of characters.
+ * No two characters may map to the same character,
+ * but a character may map to itself.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "egg", t = "add"
+ *
+ * Output: true
+ *
+ * Explanation:
+ *
+ * The strings s and t can be made identical by:
+ *
+ * Mapping 'e' to 'a'.
+ * Mapping 'g' to 'd'.
+ * Example 2:
+ *
+ * Input: s = "foo", t = "bar"
+ *
+ * Output: false
+ *
+ * Explanation:
+ *
+ * The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+ *
+ * Example 3:
+ *
+ * Input: s = "paper", t = "title"
+ *
+ * Output: true
+ *
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 5 * 104
+ * t.length == s.length
+ * s and t consist of any valid ascii character.
+ */
